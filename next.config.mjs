6f9a53628203cf1f3d4f6266e9b9b70071a56687
atharvdubey09 <dubeyatharv36@gmail.com>
2026-07-13 03:45:42 +0530

    chore: skip ts verification on build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
